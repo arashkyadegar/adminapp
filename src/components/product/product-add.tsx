ba03d16b0b0440d501a9f9d160189f4511d042ee
arashk yadegar <yadegar.arashk@gmail.com@>
2024-10-07 12:01:29 +0330

    feat: add mechanism for removing array items in forms which used tags and ...

--- src/components/product/product-add.tsx
+++ src/components/product/product-add.tsx
@@ -574,6 +574,47 @@ export default function ProductAddComponent() {
           }
 
      }
+     function removeProductExtra(value: any) {
+          const list = productFormState.data.exteras.filter((item: any) => item.name != value);
+          dispatch(
+               productFormFilled({
+                    ...productFormState.data,
+                    exteras: list
+               }))
+     }
+     function removeProductSubCategories(value: any) {
+          const list = productFormState.data.subCategories.filter((item: any) => item != value);
+          dispatch(
+               productFormFilled({
+                    ...productFormState.data,
+                    subCategories: list
+               }))
+     }
+
+     function removeProductKeywords(value: any) {
+          const list = productFormState.data.keywords.filter((item: any) => item != value);
+          dispatch(
+               productFormFilled({
+                    ...productFormState.data,
+                    keywords: list
+               }))
+     }
+     function removeProductTags(value: any) {
+          const list = productFormState.data.tags.filter((item: any) => item != value);
+          dispatch(
+               productFormFilled({
+                    ...productFormState.data,
+                    tags: list
+               }))
+     }
+     function removeProductColors(value: any) {
+          const list = productFormState.data.colors.filter((item: any) => item != value);
+          dispatch(
+               productFormFilled({
+                    ...productFormState.data,
+                    colors: list
+               }))
+     }
      function fillImageStatus(event: any): void {
           let name = event.target.value;
           let obj = productFormState.data.images.find((x: any) => x.name == name);
@@ -659,7 +700,7 @@ export default function ProductAddComponent() {
                                                        {productFormState.data.subCategories !== undefined && (
                                                             <div className="grid grid-cols-4 w-full justify-start items-start">
                                                                  {productFormState.data.subCategories.map((item: any) => (
-                                                                      <TagComponent title={item} />
+                                                                      <TagComponent title={item} onclickFunc={() => { removeProductSubCategories(item) }} />
                                                                  ))}
                                                             </div>
                                                        )}
@@ -669,9 +710,6 @@ export default function ProductAddComponent() {
                                              <div className="mb-4">
                                                   <LabelComponent name="subCategories" title="جزییات اضافی" required="true" />
                                                   <div className="flex flex-col gap-2 justify-between">
-
-
-
                                                        <div className="flex flex-row justify-between gap-2">
                                                             <div className="w-full   bg-gray-100   text-gray-900 text-sm rounded-lg  px-1">
                                                                  <input type="text" name="subCategories" placeholder="نام" ref={exterasNameRef} onKeyDown={onSubCategoriesKeyDown} className=" w-full  bg-gray-100  text-gray-900 text-sm  block  p-2.5     outline-none" />
@@ -696,10 +734,11 @@ export default function ProductAddComponent() {
                                                                            {item.name}
                                                                       </div>
                                                                       <div className="w-full   bg-white  text-gray-900 text-sm   px-1  border">
-                                                                           {item.value}                                                                           </div>
-                                                                      <div className="w-1/4 flex flex-row items-center">
+                                                                           {item.value}
+                                                                      </div>
+                                                                      <div className="w-1/4 flex flex-row items-center" onClick={() => { removeProductExtra(item.name) }} >
 
-                                                                           <a className="font-medium text-red-600 dark:text-red-500 cursor-pointer">
+                                                                           <a className="font-medium text-red-600 dark:text-red-500 cursor-pointer border">
                                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                                                                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                                                 </svg>
@@ -898,7 +937,7 @@ export default function ProductAddComponent() {
                                                        </div>
                                                        <div className="grid grid-cols-4 justify-start items-start w-full">
                                                             {productFormState.data.keywords.map((item: any) => (
-                                                                 <TagComponent title={item} />
+                                                                 <TagComponent title={item} onclickFunc={() => { removeProductKeywords(item) }} />
                                                             ))}
                                                        </div>
                                                   </div>
@@ -933,7 +972,7 @@ export default function ProductAddComponent() {
                                                        </div>
                                                        <div className="grid grid-cols-4 justify-start items-start w-full">
                                                             {productFormState.data.tags.map((item: any) => (
-                                                                 <TagComponent title={item} />
+                                                                 <TagComponent title={item} onclickFunc={() => { removeProductTags(item) }} />
                                                             ))}
                                                        </div>
                                                   </div>
@@ -1020,7 +1059,7 @@ export default function ProductAddComponent() {
                                                        </div>
                                                        <div className="grid grid-cols-4 justify-start items-start w-full">
                                                             {productFormState.data.colors.map((item: any) => (
-                                                                 <TagComponent title={item} />
+                                                                 <TagComponent title={item} onclickFunc={() => { removeProductColors(item) }} />
                                                             ))}
                                                        </div>
                                                   </div>    </div>
